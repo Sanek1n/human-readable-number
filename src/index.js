226050177231527a371133025f40f4abe2f9af01
Sanek1n <sanekin8ru@outlook.com>
2025-06-29 18:48:30 +0300

    fix spaces

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ module.exports = function toReadable (number) {
     let str = '';
     let num = String(number);
     
-    for (let i = num.length; i>0; i--) {
+    for (let i = num.length; i > 0; i--) {
         if (i==3) {
                 str += units[+num[num.length-i]-1] + ' hundred';
-        } else if (i==2) {
-                if (+num[num.length-i]==1) {
-                    (+num[num.length-i+1]==0) ? str += ' ' + decades[+num[num.length-i]-1] : str += ' ' + teens[+num[num.length-i+1]-1];
+        } else if (i == 2) {
+                if (+num[num.length-i] == 1) {
+                    (+num[num.length-i+1] == 0) ? str += ' ' + decades[+num[num.length-i]-1] : str += ' ' + teens[+num[num.length-i+1]-1];
                     i--;
                 } else {
-                    (+num[num.length-i]==0) ? str += '' : str += ' ' + decades[+num[num.length-i]-1];
+                    (+num[num.length-i] == 0) ? str += '' : str += ' ' + decades[+num[num.length-i]-1];
                 }
-        } else if (i==1) {
-            if (+num[num.length-i]==0 && str == '') {
+        } else if (i == 1) {
+            if (+num[num.length-i] == 0 && str == '') {
                 str = 'zero';
             } else {
-                (+num[num.length-i]==0) ? str += '' : str += ' ' + units[+num[num.length-i]-1];
+                (+num[num.length-i] == 0) ? str += '' : str += ' ' + units[+num[num.length-i]-1];
             };
         }
     }
